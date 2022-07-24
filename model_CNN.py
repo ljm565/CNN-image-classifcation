d41c6a3c0296e2f219853c5541ffceb22d0cbc59
Jun-Min Lee <ljm56897@gmail.com>
2022-07-24 19:44:54 +0900

    init

---
+++ model_CNN.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+from config import Config
+
+
+
+class CNN(nn.Module):
+    def __init__(self, config:Config, color_channel:int):
+        super(CNN, self).__init__()
+        self.height = config.height
+        self.width = config.width
+        self.label = config.label
+        self.color_channel = color_channel
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=self.color_channel, out_channels=32, kernel_size=7, stride=1, padding=0),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=7, stride=1, padding=0),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        self.fc = nn.Linear(64 * (self.height-12) * (self.width-12), self.label)
+
+
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.conv2(output)
+        output = torch.flatten(output, 1)
+        output = self.fc(output)
+        return output
---
+++ utils_func.py
@@ -0,0 +1,19 @@
+import torch
+from torchvision.utils import make_grid, save_image
+import os
+from PIL import Image
+from tqdm import tqdm
+import numpy as np
+import imageio
+import matplotlib.pyplot as plt
+
+
+def save_checkpoint(file, model, optimizer):
+    state = {'model': model.state_dict(), 'optimizer': optimizer.state_dict()}
+    torch.save(state, file)
+
+
+def make_img_data(path, trans):
+    files = os.listdir(path)
+    data = [trans(Image.open(path+file)) for file in tqdm(files) if not file.startswith('.')]
+    return data
